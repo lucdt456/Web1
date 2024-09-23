2cb23551b851ee179a222e1e4b6e9937ec5bea6d
TranLuc <lucdt456@gmail.com>
2024-09-23 14:57:51 +0700

    Add project files.

---
+++ Controllers/HangHoaController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web1.Data;
+using Web1.ViewModels;
+
+namespace Web1.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly Hshop2023Context db;
+
+        public HangHoaController(Hshop2023Context context)
+        {
+            db = context;
+        }
+        public IActionResult Index(int? loai)
+        {
+            var hanghoas = db.HangHoas.AsQueryable();
+
+            if (loai.HasValue) { 
+            hanghoas = hanghoas.Where(p => p.MaLoai == loai.Value);
+            }
+
+            var result = hanghoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+
+            });
+
+
+            return View(result);
+        }
+    }
+}
+
---
+++ ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web1.Data;
+using Web1.ViewModels;
+
+namespace Web1.ViewComponents
+{
+    public class MenuLoaiViewComponent : ViewComponent
+    {
+        private readonly Hshop2023Context db;
+
+        public MenuLoaiViewComponent(Hshop2023Context context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(lo => new MenuLoaiVM
+            {
+                MaLoai = lo.MaLoai,
+                TenLoai = lo.TenLoai,
+                SoLuong = lo.HangHoas.Count
+            }).OrderBy(p => p.TenLoai);
+
+            return View(data); //Default.cshtml
+            //return View("Default", data);
+        }
+    }
+}
